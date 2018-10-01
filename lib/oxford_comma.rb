724e85814b4446f53cc39cd4cf7f9082bb2c9e9c
Michael Kim <m.kim4247@gmail.com>
2018-10-01 17:29:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.shift
+  elsif array.length == 2 
+    array.join(" and ")
+    elsif array.length >= 3
+      array[0..-2].join(", ") + ", and " + array[-1]
+  end 
 end
