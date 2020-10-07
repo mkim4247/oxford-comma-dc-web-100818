9df31c8dab7f8ec9267613121e198b30eab5c88a
Michael Kim <m.kim4247@gmail.com>
2020-10-07 20:16:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
-#
+#take last item of array and put into new array
+  #give it an oxford comma
+  #convert to string
+#convert existing array into string with commas
+#concat both strings
 
 def oxford_comma(strings)
   
