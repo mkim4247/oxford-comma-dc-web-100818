13cb6fbd849a03a251b5b8a0d7eb0d8248af0b28
Michael Kim <m.kim4247@gmail.com>
2020-10-07 20:36:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
-#take last item of array and put into new array
-  #give it an oxford comma
-  #convert to string
-#convert existing array into string with commas
-#concat both strings
-
 def oxford_comma(strings)
   if strings.length > 2
     last_word = strings.pop
